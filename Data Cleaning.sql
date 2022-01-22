b26f2563e46fc63a1f94c9b66e99b0a45fc97c68
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2022-01-22 22:55:38 +0300

    Creating View for further visualization

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,35 +1,37 @@
 /* Altering Data Types for appropriate further data analysis */
 
-SET SQL_SAFE_UPDATES = 0;
-
--- Altering data type of total_deaths..CovidDeaths from TEXT To BIGINT 
-UPDATE CovidDeaths
-SET total_deaths = NULL
-WHERE total_deaths = '';
+-- Altering data type of total_deaths To BIGINT 
 
 ALTER TABLE CovidDeaths
-MODIFY COLUMN total_deaths BIGINT;
+ALTER COLUMN total_deaths
+BIGINT;
 
--- Altering data type of new_deaths..CovidDeaths from TEXT To BIGINT 
-UPDATE CovidDeaths
-SET new_deaths = NULL
-WHERE new_deaths = '';
+-- Altering data type of new_deaths To BIGINT
 
 ALTER TABLE CovidDeaths
-MODIFY COLUMN new_deaths BIGINT; 
+ALTER COLUMN new_deaths
+BIGINT; 
 
--- Altering data type of date..CovidDeaths from TEXT To DATE
+-- Altering data type of date To DATE
+
 ALTER TABLE CovidDeaths
-MODIFY COLUMN date DATE;
+ALTER COLUMN date
+DATE;
 
--- Altering data type of people_vaccinated..CovidVaccinations from TEXT To BIGINT 
-UPDATE CovidVaccinations
-SET people_vaccinated = NULL
-WHERE people_vaccinated = '';
+-- Altering data type of people_vaccinated To BIGINT
 
 ALTER TABLE CovidVaccinations
-MODIFY COLUMN people_vaccinated BIGINT;
+ALTER COLUMN people_vaccinated
+BIGINT;
 
-SET SQL_SAFE_UPDATES = 1;
+-- Altering data type of new_vaccinations To BIGINT
 
+ALTER TABLE CovidVaccinations
+ALTER COLUMN new_vaccinations
+BIGINT;
 
+-- Altering data type of date To DATE
+
+ALTER TABLE CovidVaccinations
+ALTER COLUMN date
+DATE;
--- Main.sql
+++ Main.sql
@@ -1,4 +1,4 @@
-/* Looking at global numbers */
+ /* Looking at general stats */
 
 -- Date & number of first reported cases by country
 
@@ -9,26 +9,18 @@ WHERE total_cases IS NOT NULL AND continent IS NOT NULL
 GROUP BY location
 ORDER BY date_of_first_reported_case;
 
--- Looking at the current total Covid 19 cases count & deaths
+-- Looking at the current total Covid 19 cases count
 
-SELECT location,MAX(date) AS 'current_date', MAX(total_cases) AS Total_Cases,
-	MAX(total_deaths) AS Total_Deaths
-FROM CovidDeaths
+SELECT location,MAX(date) AS 'current_date', MAX(total_cases) AS Total_Cases_Updated
+FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
-ORDER BY Total_Cases DESC;
-
--- Looking at change in global death percentage overtime
-
-SELECT YEAR(date) AS year, DATEPART(MONTH,date) AS month ,ROUND(SUM(new_deaths)/SUM(New_Cases)*100,2) as DeathPercentage
-FROM CovidDeaths
-WHERE continent is not null AND new_deaths IS NOT NULL
-GROUP BY DATEPART(MONTH,date), YEAR(date)
-ORDER BY YEAR(date),DATEPART(MONTH,date);
+ORDER BY Total_Cases_Updated DESC;
 
 /*Country Specific Stats*/
 
-SET @location = 'Egypt';
+DECLARE @location nvarchar(255)
+SET @location = 'Egypt'
 
 -- Looking at countries with highest infection rate compared to Population
 
@@ -36,7 +28,9 @@ SELECT location, MAX(DATE) AS 'current_date',
 	MAX(population) AS current_population, MAX(total_cases) AS current_total_cases,
 	ROUND((MAX(total_cases)/MAX(population))*100,2) AS current_infection_rate
 FROM CovidDeaths
-WHERE continent IS NOT NULL
+WHERE continent IS NOT NULL 
+	--AND
+	--location = @location
 GROUP BY location
 ORDER BY current_infection_rate DESC;
 
@@ -45,6 +39,8 @@ ORDER BY current_infection_rate DESC;
 SELECT location, MAX(DATE) AS 'current_date', MAX(total_deaths) AS current_total_deaths
 FROM CovidDeaths
 WHERE continent IS NOT NULL
+	--AND
+	--location = @location
 GROUP BY location
 ORDER BY current_total_deaths DESC;
 
@@ -55,7 +51,9 @@ SELECT location, MAX(DATE) AS 'current_date',
 	MAX(population) AS current_population, MAX(total_cases) AS current_total_cases,
 	ROUND((MAX(total_cases)/MAX(population))*100,2) AS current_infection_rate
 FROM CovidDeaths
-WHERE continent IS NOT NULL AND location = @location
+WHERE continent IS NOT NULL 
+	--AND
+	--location = @location
 GROUP BY location;
 
 -- Looking at the fatality rate in your country
@@ -65,12 +63,16 @@ SELECT location, MAX(DATE) AS 'current_date',
 	MAX(total_cases) AS current_totalcases, MAX(total_deaths) AS current_total_deaths,
 	ROUND((MAX(total_deaths)/MAX(total_cases))*100,2) AS current_death_percent
 FROM CovidDeaths
-WHERE continent IS NOT NULL AND location = @location
-GROUP BY location;
+WHERE continent IS NOT NULL 
+	--AND 
+	--location = @location
+GROUP BY location
+ORDER BY current_death_percent DESC, current_total_deaths DESC;
 
 -- Looking at Continent Specific Stats
 
-SET @continent  =  'Africa';
+DECLARE @continent nvarchar(255)
+SET @continent = 'Africa'
 
 -- Looking at continents with highest death count per population
 
@@ -78,14 +80,45 @@ WITH countrydeathcount AS (SELECT continent,location,
 	MAX(total_deaths) AS new_total
 FROM CovidDeaths
 WHERE continent IS NOT NULL 
-	--AND continent = @continent
 GROUP BY location, continent
 )
 SELECT continent, SUM(new_total) AS deaths_per_continent
 FROM countrydeathcount
+-- WHERE
+	--AND 
+	--continent = @continent
 GROUP BY continent
 ORDER BY deaths_per_continent DESC;
 
 -- Looking at Total Population Vs Vaccinations Per Country
+
+WITH POPVSVAC AS (
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(vac.new_vaccinations) OVER 
+	(PARTITION BY dea.Location ORDER BY dea.location, dea.Date) AS RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null 
+)
+SELECT continent, location, date, (RollingPeopleVaccinated/Population)/100 AS PercentVaccinated
+FROM POPVSVAC
+--WHERE location = @location 
+ORDER BY location, date;
+
+-- Creating View to store data for later visualizations
+
+CREATE VIEW PopVsVac 
+AS 
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(vac.new_vaccinations) OVER 
+	(PARTITION BY dea.Location ORDER BY dea.location, dea.Date) AS RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null;
+
 
 
